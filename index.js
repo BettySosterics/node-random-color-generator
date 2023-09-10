d8f4f17f041b9ea38139c004b9edc3805e000bc1
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-10 19:10:43 +0200

    Add hue and luminosity option for user

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import { argv } from 'node:process';
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const color = randomColor();
+const color = randomColor({ hue: argv[2], luminosity: argv[3] });
 // console.log(color);
 
 console.log(
-  chalk.hex(color)(`
+  chalk.hex(color).bold(`
   ##############################
   ##############################
   ########              ########
